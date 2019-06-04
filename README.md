a4aee083126e3615ff085fcd0ae1e69be10e8d04
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-06-04 19:31:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# app_buscador_gifs
---
+++ android/app/src/main/java/appbuscadorgifs/lrs/com/br/app_buscador_gifs/MainActivity.java
@@ -0,0 +1,13 @@
+package appbuscadorgifs.lrs.com.br.app_buscador_gifs;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
